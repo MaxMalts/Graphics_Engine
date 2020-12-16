721eb3189ce4eac682c755440346e28c48128816
MaxMalts <max-kurt@bk.ru>
2020-12-16 17:14:53 +0300

    Started GraphicsEditor

---
+++ ExamplePrograms/GraphicsEditor/GraphicsEditor.cpp
@@ -0,0 +1,17 @@
+﻿#include <GraphicsEngine.h>
+
+
+
+int main() {
+
+	GUI::Application app;
+	GUI::OSWindow* osWindow = app.CreateWindow(1600, 900, "Graphics Editor", GUI::Color("white"));
+	GUI::DesktopWindow* desktop = osWindow->GetDesktop();
+
+
+	while (true) {
+		app.ProcessEvents();
+	}
+
+	return 0;
+}
